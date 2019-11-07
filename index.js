34624846cbb33aec00489f17075316b281e6b07b
Zach <zachcbruno@gmail.com>
2019-11-07 12:32:55 -0800

    Fixing up dependencies

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-module.exports = () => ({
-  presets: [
-    '@babel/preset-env',
-    '@babel/preset-react',
-  ],
-  plugins: [
-    ["@babel/plugin-proposal-decorators", { decoratorsBeforeExport: true }],
-    "@babel/plugin-proposal-class-properties",
-    '@babel/plugin-proposal-export-default-from',
-    '@babel/plugin-proposal-nullish-coalescing-operator',
-    '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-proposal-export-namespace-from',
-    '@babel/plugin-proposal-throw-expressions',
-    '@babel/plugin-syntax-dynamic-import',
-    'mobx-deep-action'
-  ].filter(Boolean),
-});
+module.exports = function() {
+  return {
+    presets: [
+      '@babel/preset-env',
+      '@babel/preset-react',
+    ],
+    plugins: [
+      ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-export-default-from',
+      '@babel/plugin-proposal-nullish-coalescing-operator',
+      '@babel/plugin-proposal-object-rest-spread',
+      '@babel/plugin-proposal-export-namespace-from',
+      '@babel/plugin-proposal-throw-expressions',
+      '@babel/plugin-syntax-dynamic-import',
+      'mobx-deep-action',
+    ].filter(Boolean),
+  };
+};
