b98769a06fc472ba7ef2f79df6b0060475fdfeb9
Zach <zachcbruno@gmail.com>
2019-11-07 09:57:57 -0800

    Fix babel issues
    Need to make sure to use babel loader on projects with this preset

--- index.js
+++ index.js
@@ -4,18 +4,13 @@ module.exports = () => ({
     '@babel/preset-react',
   ],
   plugins: [
+    ["@babel/plugin-proposal-decorators", { decoratorsBeforeExport: true }],
+    "@babel/plugin-proposal-class-properties",
     '@babel/plugin-proposal-export-default-from',
-    ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: false }],
-
-    // Stage-2
+    '@babel/plugin-proposal-nullish-coalescing-operator',
     '@babel/plugin-proposal-object-rest-spread',
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
     '@babel/plugin-proposal-export-namespace-from',
     '@babel/plugin-proposal-throw-expressions',
-
-    // Stage-3
     '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-syntax-import-meta',
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
   ],
 });
