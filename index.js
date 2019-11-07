add2588ce148b6f281a60e2b068049a18973048b
Zach <zachcbruno@gmail.com>
2019-11-06 17:51:01 -0800

    Create shareable babel config

---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = () => ({
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+    'babel-preset-jest',
+  ],
+  plugins: [
+    '@babel/plugin-proposal-export-default-from',
+    ['@babel/plugin-proposal-optional-chaining', { loose: false }],
+    ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: false }],
+
+    // Stage-2
+    '@babel/plugin-proposal-object-rest-spread',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    '@babel/plugin-proposal-export-namespace-from',
+    '@babel/plugin-proposal-throw-expressions',
+
+    // Stage-3
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-syntax-import-meta',
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+  ],
+});
