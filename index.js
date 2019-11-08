15bdc65209d866a521b08c6d6c0d8fb22e53cdb2
Zach <zachcbruno@gmail.com>
2019-11-08 13:18:52 -0800

    Fixing problem

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ module.exports = function() {
       '@babel/preset-react',
     ],
     plugins: [
-      ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
-      '@babel/plugin-proposal-class-properties',
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      '@babel/plugin-transform-runtime',
       '@babel/plugin-proposal-export-default-from',
       '@babel/plugin-proposal-nullish-coalescing-operator',
       '@babel/plugin-proposal-object-rest-spread',
