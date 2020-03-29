b95d2a9508745b12e30a61d8f8cb7450354adc87
Zach <zachcbruno@gmail.com>
2020-03-29 12:09:27 -0700

    Cleaning up config

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 module.exports = function() {
   return {
     presets: [
-      '@babel/preset-env',
-      '@babel/preset-react',
+      ['react-app', { 'flow': false, 'typescript': true }],
       'mobx',
+      '@emotion/babel-preset-css-prop'
     ],
     plugins: [
-      ['@babel/plugin-proposal-decorators', { legacy: true }],
-      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      'emotion',
+      'macros',
       '@babel/plugin-transform-runtime',
       '@babel/plugin-proposal-export-default-from',
-      '@babel/plugin-proposal-nullish-coalescing-operator',
       '@babel/plugin-proposal-object-rest-spread',
       '@babel/plugin-proposal-export-namespace-from',
       '@babel/plugin-proposal-throw-expressions',
-      '@babel/plugin-syntax-dynamic-import',
       'mobx-deep-action',
     ].filter(Boolean),
   };
